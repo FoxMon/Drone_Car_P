4e89e62ca8c59194319395774bff6e309c7f20f8
ChaePIL <jhpark4884@naver.com>
2020-03-18 17:21:15 +0900

    Drone_Ver02_1/bluetooth.ino

--- Drone_Ver02_1/bluetooth.ino
+++ Drone_Ver02_1/bluetooth.ino
@@ -10,12 +10,28 @@ void Blue_Setup(){
   bluetooth.begin(9600);
 }
 
-void Remote_Drone(){
+void Remote_Drone(Throttle &throttle , TargetAngle &target){
   if(bluetooth.available()){
     dr_ctrl = bluetooth.parseInt(); // 일단 정수로 받자.
 
     if(dr_ctrl >= 0 && dr_ctrl <= 25){
       throttle.value = (dr_ctrl) * 10; //min 0 ~ max 250
+    }
+   
+    if(dr_ctrl >=30 && dr_ctrl <= 35){
+
+      if(dr_ctrl == 30){
+        target.pitch = 10;
+      }
+      else if(dr_ctrl == 31){
+        target.pitch = (-10);
+      }
+      else if(dr_ctrl == 32){
+        target.roll = 10;
+      }
+      else if(dr_ctrl == 33){
+        target.roll = (-10);
+      }
     }
   }
 }
